d3b2297391a082d307d62ec44319eb6e469b0bf7
vldan <vlad.danilov.020212@gmail.com>
2023-04-03 09:27:52 +0300

    hotFix: Правильно подключить шрифты, Изменить кнопку сохранения pop-up, удалить style.css, создать папку scripts, создать обертку для pop-up

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,16 @@ let popUp = document.querySelector('.pop-up')
 let editButton = document.querySelector('.profile-info__edit-button')
 let closeButton = document.querySelector('.pop-up__form-button-close')
 
+let popUpName = document.querySelector('.pop-up__form-input-name')
+let popUpProfession = document.querySelector('.pop-up__form-input-profession')
+let profileName = document.querySelector('.profile-info__name')
+let profileProfession = document.querySelector('.profile-info__profession')
+
+let saveButton = document.querySelector('.pop-up__form-button-save')
+
+popUpName.value = profileName.textContent;
+popUpProfession.value = profileProfession.textContent;
+
 function openPopUp(){
     popUp.classList.add('pop-up_opened')
 }
@@ -14,22 +24,11 @@ function closePopUp(){
 
 closeButton.addEventListener('click', closePopUp)
 
-
-let popUpName = document.querySelector('.pop-up__form-input-name')
-let popUpProfession = document.querySelector('.pop-up__form-input-profession')
-let profileName = document.querySelector('.profile-info__name')
-let profileProfession = document.querySelector('.profile-info__profession')
-
-popUpName.value = profileName.textContent;
-popUpProfession.value = profileProfession.textContent;
-
-
-let saveButton = document.querySelector('.pop-up__form-button-save')
-
-function savePopUp(){
+function savePopUp(event){
+    event.preventDefault();
     profileName.textContent = popUpName.value;
     profileProfession.textContent = popUpProfession.value;
-    closePopUp();
+    closePopUp()
 }
 
 saveButton.addEventListener('click', savePopUp)
